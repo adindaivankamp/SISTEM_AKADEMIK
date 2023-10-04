a5e5c3e996b51b6688b32a10298cf54a17b50883
adindaivankamp <145529737+adindaivankamp@users.noreply.github.com>
2023-10-04 13:56:38 +0700

    update tugas 3

---
+++ Tugas-3.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Daspro_Jobsheet3 {
+    public static void main (String[]args){
+        Scanner sc = new Scanner(System.in);
+        int NilaiTugas, NilaiKuis, NilaiUTS, NilaiUAS;
+        double RataRata;
+
+        System.out.println("Masukkan nilai tugas");
+        NilaiTugas = sc.nextInt();
+        System.out.println("Masukkan nilai kuis");
+        NilaiKuis = sc.nextInt();
+        System.out.println("Masukkan nilai UTS");
+        NilaiUTS = sc.nextInt();
+        System.out.println("Masukkan nilai UAS");
+        NilaiUAS = sc.nextInt();
+
+        RataRata = (NilaiTugas + NilaiKuis + NilaiUTS + NilaiUAS) / 4;
+
+        System.out.println("Rata - rata = " + RataRata);
+        
+    }
+}
