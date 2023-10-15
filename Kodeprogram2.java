6cc553dc3fbf0b4b08814d8b1bb92f5998523fc1
adindaivankamp <145529737+adindaivankamp@users.noreply.github.com>
2023-10-15 21:18:13 +0700

    Update

---
+++ Kodeprogram2.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Kodeprogram2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int jumlahSiswa, nilai, totalNilai = 0;
+        double rataRata;
+
+        System.out.print("Masukkan jumlah siswa: ");
+        jumlahSiswa = input.nextInt();
+
+        if (jumlahSiswa <= 0) {
+            System.out.println("Jumlah siswa harus lebih dari 0.");
+        } else {
+            for (int i = 0; i < jumlahSiswa; i++) {
+                System.out.print("Masukkan nilai siswa ke-" + (i + 1) + ": ");
+                nilai = input.nextInt();
+                totalNilai += nilai;
+            }
+
+            rataRata = (double) totalNilai / jumlahSiswa;
+
+            System.out.println("Rata-rata nilai: " + rataRata);
+        }
+    }
+}
+
