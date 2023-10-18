ec984bed048c138837a4a35d6bbb5744c16f45dc
adindaivankamp <145529737+adindaivankamp@users.noreply.github.com>
2023-10-18 09:47:36 +0700

    Tugas 1

--- SistemAkademik1.java
+++ SistemAkademik1.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Tugas1 {
+public class SistemAkademik1 {
     public static void main (String[]args){
         Scanner sc = new Scanner(System.in);
         int NilaiTugas, NilaiKuis, NilaiUTS, NilaiUAS;
